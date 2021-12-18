0d2524b7c09ea725882081b710ecef22eb7f10ff
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2021-12-17 21:51:36 -0300

    Projeto BLOCO 1

---
+++ src/ProjetoDoeBahia/TesteDoador.java
@@ -0,0 +1,15 @@
+package ProjetoDoeBahia;
+
+public class TesteDoador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ProjetoDoador projeto=new ProjetoDoador (null,null,null,null,null,null,null,null,null,null,null,0);
+		projeto.imprimirInfo();
+		projeto.doadores();
+
+	}
+
+}
+
